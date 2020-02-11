d3fd40f0ab925d71490882fdaa9f108c62f46511
sachin <sachin@localhost.localdomain>
2020-02-11 17:20:18 +0530

    added azure ad authentication

---
+++ Config.js
@@ -0,0 +1,7 @@
+
+const Config = {
+    client_id: '10dc069d-a3b3-4138-bfb0-17c2f2257d4f',
+    scope: 'User.ReadBasic.All User.Read.All User.ReadWrite.All Mail.Read offline_access'
+  };
+
+export default Config
--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,18 +1,104 @@
-// Homescreen.js
-import React, { Component } from 'react';
-import { Button, View, Text } from 'react-native';
-import { createAppContainer } from 'react-navigation';
+import React from 'react';
+import { StyleSheet, Text, View,Button } from 'react-native';
+import {AzureInstance, AzureLoginView} from '../lib';
+import CookieManager from 'react-native-cookies';
+import Config from '../Config'
 
-export default class HomeScreen extends Component {
+const credentials = {
+  client_id: Config.client_id,
+  scope: Config.scope
+};
+
+export default class LoginScreen extends React.Component {
+    static navigationOptons ={
+        title : 'Welcome'
+    }
+  constructor(props){
+    super(props);
+    this.state = {
+      azureLoginObject: {},
+      loginSuccess: false,
+      events :[],
+    };
+		this.azureInstance = new AzureInstance(credentials);
+		this._onLoginSuccess = this._onLoginSuccess.bind(this);
+	}
+
+	_onLoginSuccess(){
+		this.azureInstance.getUserInfo().then(result => {
+			this.setState({
+        loginSuccess: true,
+        azureLoginObject: result
+      });
+      console.log(result);
+		}).catch(err => {
+			console.log(err);
+		})
+  }
+
+  
+  _logout(){
+
+    this.setState({
+      loginSuccess:false,
+    });
+    CookieManager.clearAll();
+
+  }
+
+  get_events(){
+		this.azureInstance.get_events().then(json => {
+      a =[json.value[0].subject,json.value[1].subject]
+			this.setState({
+        events: a
+      });
+
+      console.log(this.state.events[0]);
+
+		}).catch(err => {
+			console.log(err);
+		})
+  }
+
   render() {
+    
+    if (!this.state.loginSuccess) {
+
+      return (<AzureLoginView
+          azureInstance={this.azureInstance}
+          loadingMessage="Requesting access token"
+          onSuccess={this._onLoginSuccess}
+          excludeSecretFromTokenRequest={true}
+        />)
+    }
+
+    const {userPrincipalName, givenName} = this.state.azureLoginObject;
+    
+    
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-          <Button
-          title="Go to About"
-          onPress={() => this.props.navigation.navigate('Map')}
-          />
-      </View>
-    )
+      <View style={styles.container}>
+				<Text style={styles.text}>Welcome {givenName}</Text> 
+				<Text style={styles.text}>You logged into Azure with {userPrincipalName}</Text> 
+        <Button title="Logout" onPress={(e)=>this._logout()}/>
+        <Button title="Events" onPress={(e)=>this.get_events()}/>
+        <Text>{this.state.events[0]}</Text>
+        <Text>{this.state.events[1]}</Text>
+
+			</View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+		textAlign: 'center',
+		color: '#333333',
+		marginBottom: 5
+	}
+});
